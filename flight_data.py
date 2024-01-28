252ae8319f145a1ce9186762ab13cccc0d641e2c
CristinaAlina <sirbu.cristina.alina94@gmail.com>
2024-01-29 01:55:57 +0200

    Created FlightData class which contains only a constructor with defined parameters for how a FlightData object whould look like

--- flight_data.py
+++ flight_data.py
@@ -1,3 +1,11 @@
 class FlightData:
-    #This class is responsible for structuring the flight data.
-    pass
+    # This class is responsible for structuring the flight data.
+
+    def __init__(self, price, from_airport_code, from_city, to_airport_code, to_city, departure_date, arrival_date):
+        self.price = price
+        self.from_airport_code = from_airport_code
+        self.from_city = from_city
+        self.to_airport_code = to_airport_code
+        self.to_city = to_city
+        self.departure_date = departure_date
+        self.arrival_date = arrival_date
