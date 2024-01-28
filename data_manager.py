024cabee69cac49b9b3182f137fb0ab2ab0b21c9
CristinaAlina <sirbu.cristina.alina94@gmail.com>
2024-01-29 01:53:09 +0200

    Created DataManager class which contains methods for getting data from Google Sheets target file and update specific row with new iataCode, both using Sheety API with GET and PUT methods

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,34 @@
+import requests
+import os
+
+SHEETY_GET_ENDPOINT = os.environ.get("SHEETY_GET_ENDPOINT")
+
+HEADERS = {
+    "Authorization": f"Bearer {os.environ.get("SHEETY_BEARER_TOKEN")}"
+}
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    # This class is responsible for talking to the Google Sheet.
+
+    def __init__(self):
+        self.prices_data = []
+
+    def get_data(self):
+        """Returns a list with all the data found into Google Sheets target sheet."""
+        response = requests.get(url=SHEETY_GET_ENDPOINT, headers=HEADERS)
+        json_data = response.json()
+
+        self.prices_data = json_data["prices"]
+        return self.prices_data
+
+    def update_sheet_iata_code(self, iata_code, city_name):
+        """Update a specific row from Google Sheets target sheet, with a new iataCode"""
+        id_row = [data_row['id'] for data_row in self.prices_data if data_row["city"] == city_name][0]
+
+        update_body = {
+            "price": {
+                "iataCode": iata_code
+            }
+        }
+        requests.put(url=f"{SHEETY_GET_ENDPOINT}/{id_row}", json=update_body, headers=HEADERS)
