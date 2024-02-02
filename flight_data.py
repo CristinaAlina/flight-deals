90374410b74bf02fdcb7a5314ad560dc2778928b
CristinaAlina <sirbu.cristina.alina94@gmail.com>
2024-02-02 10:29:08 +0200

    Added 2 new arguments, stop_overs and via_city in case of existing flights with 1 stop instead of direct flights

--- flight_data.py
+++ flight_data.py
@@ -1,7 +1,8 @@
 class FlightData:
     # This class is responsible for structuring the flight data.
 
-    def __init__(self, price, from_airport_code, from_city, to_airport_code, to_city, departure_date, arrival_date):
+    def __init__(self, price, from_airport_code, from_city, to_airport_code, to_city, departure_date, arrival_date,
+                 stop_overs=0, via_city=""):
         self.price = price
         self.from_airport_code = from_airport_code
         self.from_city = from_city
@@ -9,3 +10,5 @@ class FlightData:
         self.to_city = to_city
         self.departure_date = departure_date
         self.arrival_date = arrival_date
+        self.stop_overs = stop_overs
+        self.via_city = via_city
